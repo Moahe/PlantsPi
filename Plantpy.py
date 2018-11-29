e0f61872d94d5975611d3b8a0d9ca4cc3af2b0e0
Moa Hermansson <id14mhn@cs.umu.se>
2018-11-29 11:25:15 +0100

    Add files via upload

---
+++ Plantpy.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import smtplib
+import time
+
+"""Test for raspberry pi with sensor"""
+
+
+def callback(channel):
+    if GPIO.input(channel):
+        print("LED OFF")
+    else:
+        print("LED ON")
+
+
+GPIO.setmode(GPIO.BCM)
+channel = 17
+GPIO.setup(channel, GPIO.IN)
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(channel, callback)
+
+while True:
+    time.sleep(1)
