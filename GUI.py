041177b1d598ad1a9153518649dc6ce5943dc61e
id14mhn@cs.umu.se <Majsan92>
2018-12-03 14:37:22 +0100

    Second commit

--- GUI.py
+++ GUI.py
@@ -1,18 +1,27 @@
 
 import tkinter
 
+tk = tkinter.Tk()
+
+
+def callback():
+    print("Click")
+
 
 def GUI(str, strinfo):
-    tk = tkinter.Tk()
+
     tk.resizable(0, 0)
     tk.configure(background='white')
     #tk.attributes('-fullscreen', True)
     tk.title("PlantPy")
     tk.geometry("800x480")
     lbltitle = tkinter.Label(tk, text=str, font=("Arial Bold", 30), background="white")
-    lbltitle.place(x = 250, y= 120)
+    lbltitle.place(x=230, y=100)
     lblinfo = tkinter.Label(tk, text=strinfo, font=("Arial Bold", 15), background="white")
     lblinfo.place(x=250, y=240)
+    b = tkinter.Button(tk, text="x",command=callback)
+    b.place(x=780, y=450)
+    b.bind()
     back = tkinter.Frame(master=tk, bg='white')
     tk.mainloop()
 
