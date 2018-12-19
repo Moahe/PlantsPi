51f140315c13e25179f220098f6646afac4f529c
id14mhn@cs.umu.se <Majsan92>
2018-12-19 13:19:36 +0100

    third commit conn to api

--- GUI.py
+++ GUI.py
@@ -1,27 +1,60 @@
 
-import tkinter
+try:
+    import Tkinter as tkinter
+
+except(ModuleNotFoundError):
+    import tkinter as tkinter
 
 tk = tkinter.Tk()
 
-
-def callback():
-    print("Click")
-
-
 def GUI(str, strinfo):
+
 
     tk.resizable(0, 0)
     tk.configure(background='white')
     #tk.attributes('-fullscreen', True)
     tk.title("PlantPy")
     tk.geometry("800x480")
+    """
     lbltitle = tkinter.Label(tk, text=str, font=("Arial Bold", 30), background="white")
     lbltitle.place(x=230, y=100)
     lblinfo = tkinter.Label(tk, text=strinfo, font=("Arial Bold", 15), background="white")
     lblinfo.place(x=250, y=240)
-    b = tkinter.Button(tk, text="x",command=callback)
-    b.place(x=780, y=450)
     b.bind()
     back = tkinter.Frame(master=tk, bg='white')
+    """
+    C = tkinter.Canvas(tk, bg="blue", height=800, width=480)
+    filename = tkinter.PhotoImage(file="1.png")
+    background_label = tkinter.Label(tk, image=filename)
+    background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+
+    def inc():
+        korv = open('num.txt', 'r+')
+        i = korv.read()
+        korv.close()
+        korv = open('num.txt', 'w+')
+
+        y = int(i)
+        print(y)
+        y = y+1
+        if y > 10:
+            y = 1
+
+        korv.write((y.__str__()))
+        korv.close()
+        return y
+
+    def callback():
+        quit()
+
+    def pic():
+
+        #img2 = tkinter.PhotoImage(file="1.png")
+        filename.configure(file=("%i.png" % inc()))
+
+    b = tkinter.Button(tk, text="    ", command=pic, bg="white")
+
+    b.place(x=760, y=440)
     tk.mainloop()
 
--- lora.py
+++ lora.py
@@ -1,21 +1,44 @@
 import json
 import requests
 import urllib3
-import tkinter
 
 from GUI import GUI
 """Teeest"""
-file_object = open("loraurl.txt", "r")
-plane = '3'
-sensor = '3'
-url = file_object.read()
-stringen = '%s' %url
-headers = {'content-type': 'application/json'}
-response = requests.get(url, headers)
-data = response.json()
-print(data[0]['dd'])
-str = 'Våning %s sensor %s:' % (plane, sensor)
-strinfo = 'Tid: %s\n timestamp: %s\n Temperature: %s\nHumidity: %s\nLight: %s\n' % (data[0]['time'], data[0]['ts'], data[0]['dd']['temperature'], data[0]['dd']['humidity'], data[0]['dd']['light'])
-strinfo = strinfo+'Movement: %s\n Battery: %s\n' %( data[0]['dd']['pir'],  data[0]['dd']['vdd'])
 
+str = ""
+strinfo = ""
+
+def loraAPIconnection():
+    file_object = open("loraurl.txt", "r")
+    plane = '3'
+    sensor = '3'
+    url = file_object.read()
+    stringen = '%s' %url
+    headers = {'content-type': 'application/json'}
+    response = requests.get(url, headers)
+    data = response.json()
+    print(data[0]['dd'])
+    str = 'Våning %s sensor %s:' % (plane, sensor)
+    strinfo = 'Tid: %s\n timestamp: %s\n Temperature: %s\nHumidity: %s\nLight: %s\n' % (data[0]['time'], data[0]['ts'], data[0]['dd']['temperature'], data[0]['dd']['humidity'], data[0]['dd']['light'])
+    strinfo = strinfo+'Movement: %s\n Battery: %s\n' %( data[0]['dd']['pir'],  data[0]['dd']['vdd'])
+
+
+def getsensorAPI():
+    url = "http://webplant.azurewebsites.net/api/soilsensordatas"
+    headers = {'content-type': 'application/json'}
+    response = requests.get(url, headers).json()
+
+    return response
+
+
+def postToSensorAPI():
+    url = "http://webplant.azurewebsites.net/api/soilsensordatas"
+    dir = {'MoistInt': 1, 'timestamp': 1337}
+    headers = {'content-type': 'application/json'}
+    post = requests.post(url, data=json.dumps(dir), headers=headers).json()
+
+    return post
+
+
+#print(getsensorAPI())
 GUI(str, strinfo)
